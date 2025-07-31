6ca97549445614400a734585d078d6750b34e611
MunizErthal <32694606+MunizErthal@users.noreply.github.com>
2025-07-31 17:43:46 -0300

    Inclusion of project green goblin infos and layout to project info pages.

--- src/app/experiences/experiences.ts
+++ src/app/experiences/experiences.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '../../pipes/translate.pipe';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-experiences',
-  imports: [],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './experiences.html',
-  styleUrl: './experiences.scss'
+  styleUrls: ['./experiences.scss', './experiences-mobile.scss'],
 })
 export class Experiences {
 
+  language: string = 'en';  // en ou pt
+  mobileMenu = false;
+
+  
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private languageService: LanguageService) {
+    this.subscribeToLanguageChanges();
+    this.verifyMobile();
+    this.loadLanguage();
+  }
+
+  subscribeToLanguageChanges() {
+    this.languageService.language$.subscribe(lang => {
+      this.language = lang;
+    });
+  }
+
+  loadLanguage() {
+    this.route.queryParams.subscribe(params => {
+      var languageParam = params['language'];
+      if (languageParam)
+        this.language = languageParam;
+    });
+  }
+
+  verifyMobile() {
+    if (navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      this.mobileMenu = true;
+    }
+  }
 }
